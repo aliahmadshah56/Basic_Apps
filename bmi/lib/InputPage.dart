d43aed9c4d639a27f2632025a7a1fef454fd01e6
aliahmadshah56 <www.a7726867@gmail.com>
2024-07-25 00:39:37 -0700

    Gesture Detector

--- bmi/lib/InputPage.dart
+++ bmi/lib/InputPage.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'container.dart';
 import 'iconandtext.dart';
 
+const activeColor = Color(0xFF1D1E33);
+const deActiveColor = Color(0xFF111328);
 
 class Input extends StatefulWidget {
   const Input({super.key});
@@ -12,59 +14,83 @@ class Input extends StatefulWidget {
 }
 
 class _InputState extends State<Input> {
+  Color maleColor = deActiveColor;
+  Color femaleColor = deActiveColor;
+
+  void updateColor(int gen) {
+    if (gen == 1) {
+      maleColor = activeColor;
+      femaleColor = deActiveColor;
+    }
+    if (gen == 2) {
+      maleColor = deActiveColor;
+      femaleColor = activeColor;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: RepaeatContainer(
-                      color: Color(0xFF1D1E33),
-                       cardWidget:  columnWidget(ico: Icons.male, txt: 'MALE',),
+        appBar: AppBar(
+          title: Text('BMI Calculator'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          updateColor(1);
+                        });
+                      },
+                      child: RepaeatContainer(
+                        color: maleColor,
+                        cardWidget: columnWidget(
+                          ico: Icons.male,
+                          txt: 'MALE',
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: RepaeatContainer(
-                      color: Color(0xFF1D1E33),
-                    cardWidget:  columnWidget(ico: Icons.female,txt: "FEMALE",),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          updateColor(2);
+                        });
+                      },
+                      child: RepaeatContainer(
+                        color: femaleColor,
+                        cardWidget: columnWidget(
+                          ico: Icons.female,
+                          txt: "FEMALE",
+                        ),
+                      ),
+                    ),
                   ),
-
-                ),
-              ],
-            ),
-          ),
-
-          Expanded(
-              child: new RepaeatContainer(
-                  color: Color(0xFF1D1E33),
-              )
-          ),
-          Expanded(
-            child: Row(
-            children: [
-              Expanded(
-                  child: new RepaeatContainer(
-                      color: Color(0xFF1D1E33),
-                    )
-            ),
-              Expanded(
-                  child:new RepaeatContainer(color: Color(0xFF1D1E33))
+                ],
               ),
-
-
-            ],
-          ),),
-        ],
-      )
-    );
+            ),
+            Expanded(
+                child: new RepaeatContainer(
+              color: Color(0xFF1D1E33),
+            )),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: new RepaeatContainer(
+                    color: Color(0xFF1D1E33),
+                  )),
+                  Expanded(
+                      child: new RepaeatContainer(color: Color(0xFF1D1E33))),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
-
-
-
