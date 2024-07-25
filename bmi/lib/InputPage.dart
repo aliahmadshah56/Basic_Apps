2fcdcfb64ba78e6999fe28037f8dd6be53865616
aliahmadshah56 <www.a7726867@gmail.com>
2024-07-25 00:19:53 -0700

    Repeated Column Widget

--- bmi/lib/InputPage.dart
+++ bmi/lib/InputPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class Input extends StatefulWidget {
   const Input({super.key});
 
@@ -20,23 +21,34 @@ class _InputState extends State<Input> {
             child: Row(
               children: [
                 Expanded(
-                  child: RepaeatContainer(color: Color(0xFF1D1E33)),
+                  child: RepaeatContainer(
+                      color: Color(0xFF1D1E33),
+                       cardWidget:  columnWidget(ico: Icons.male, txt: 'MALE',),
+                  ),
                 ),
                 Expanded(
-                  child: RepaeatContainer(color: Color(0xFF1D1E33)),
+                  child: RepaeatContainer(
+                      color: Color(0xFF1D1E33),
+                    cardWidget:  columnWidget(ico: Icons.female,txt: "FEMALE",),
+                  ),
+
                 ),
               ],
             ),
           ),
 
           Expanded(
-              child: new RepaeatContainer(color: Color(0xFF1D1E33))
+              child: new RepaeatContainer(
+                  color: Color(0xFF1D1E33),
+              )
           ),
           Expanded(
             child: Row(
             children: [
               Expanded(
-                  child: new RepaeatContainer(color: Color(0xFF1D1E33))
+                  child: new RepaeatContainer(
+                      color: Color(0xFF1D1E33),
+                    )
             ),
               Expanded(
                   child:new RepaeatContainer(color: Color(0xFF1D1E33))
@@ -51,15 +63,42 @@ class _InputState extends State<Input> {
   }
 }
 
-class RepaeatContainer extends StatelessWidget {
-  final Color color;
+class columnWidget extends StatelessWidget {
+ final String txt;
+ final IconData ico;
 
-  const RepaeatContainer({required this.color});
+ columnWidget({required this.txt, required this.ico});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          ico,
+          size: 100,
+        ),
+      SizedBox(height: 10,),
+      Text(txt,
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold
+      ),)],
+    );
+  }
+}
+
+class RepaeatContainer extends StatelessWidget {
+
+  RepaeatContainer({required this.color, this.cardWidget});
+  final Color color;
+  final Widget? cardWidget;
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15),
+      child: cardWidget,
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(10),
